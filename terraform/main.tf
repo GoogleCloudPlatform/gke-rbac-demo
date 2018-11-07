13b60e6ae6fc83d6fac8573fe0d8e1711140d7e7
Hari Kallam <hari.kallam@yahoo.co.uk>
2018-11-07 11:39:04 -0600

    changes to the main.tf and author (#9)

--- terraform/main.tf
+++ terraform/main.tf
@@ -163,15 +163,3 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-# The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.primary.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-}
